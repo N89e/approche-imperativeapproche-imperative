6e5de50d449cb097c1631aada8e4ed101a2d2dda
N89e <nunodafonseca@live.fr>
2025-07-07 11:50:54 +0200

    ajout main ainsi qu'une class

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,10 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        String a = "Bonjour";
+        System.out.println(a);
+        String randomString = "Voici le résultat d’un calcul : 1+5=6";
+        System.out.println(randomString);
+    }
+}
