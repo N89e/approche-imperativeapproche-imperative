4ef962d8dfb8f3db31568fb3277677ea64a00b2a
N89e <nunodafonseca@live.fr>
2025-07-07 14:55:56 +0200

    ajout ajout une autre façon d'afficher tous les éléments d'un tableau

--- src/fr/tableaux/ExerciceTTableauChaines.java
+++ src/fr/tableaux/ExerciceTTableauChaines.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 public class ExerciceTTableauChaines {
     public static void main(String[] args) {
-        String[] tableauString = {"Paris","Porto","Monaco","Nantes","Marseille"};
-        Arrays.stream(tableauString).forEach(System.out::println);
+        String[] tableauString = {"Paris", "Porto", "Monaco", "Nantes", "Marseille"};
+        for (String s : tableauString) {
+            System.out.println("Ville :" + s);
+        }
+        ;
         System.out.println("Longueur tableau :" + tableauString.length);
         tableauString[3] = "Reims";
         Arrays.stream(tableauString).forEach(System.out::println);
